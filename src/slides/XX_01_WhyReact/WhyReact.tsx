ed864fed647a6a4315387c6a8715f1006dc8cf25
Frank González Hernández <caox2110@gmail.com>
2023-09-06 19:01:44 +0200

    chore: update slide why react

--- src/slides/XX_01_WhyReact/WhyReact.tsx
+++ src/slides/XX_01_WhyReact/WhyReact.tsx
@@ -1,10 +1,18 @@
-import { Heading } from 'spectacle'
-import { NormalSlide } from '../../components'
+import { FlexBox, Heading, Slide } from 'spectacle'
+import Back from '../../assets/images/back.png'
 
 export function WhyReact() {
   return (
-    <NormalSlide>
-      <Heading>¿Qué le ofrece Grafana Faro a React?</Heading>
-    </NormalSlide>
+    <Slide backgroundImage={`url(${Back})`}>
+      <FlexBox
+        width="100%"
+        height="100%"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="flex-start"
+      >
+        <Heading>¿Por qué React?</Heading>
+      </FlexBox>
+    </Slide>
   )
 }
