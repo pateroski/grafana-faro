d86aab1cfe3861f98090a422d613824fac025ec1
Frank González Hernández <caox2110@gmail.com>
2023-09-06 17:28:16 +0200

    chore: add why react slides

---
+++ src/slides/XX_01_WhyReact/WhyReact.tsx
@@ -0,0 +1,10 @@
+import { Heading } from 'spectacle'
+import { NormalSlide } from '../../components'
+
+export function WhyReact() {
+  return (
+    <NormalSlide>
+      <Heading>¿Qué le ofrece Grafana Faro a React?</Heading>
+    </NormalSlide>
+  )
+}
---
+++ src/slides/XX_01_WhyReact/index.ts
@@ -0,0 +1,1 @@
+export { WhyReact } from './WhyReact';
