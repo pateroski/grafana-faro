4bdd03c2bd30e669abbec6933d1844dc8cbd1ffb
Andrés Reyes <[p4ter0ski@gmail.com](mailto:p4ter0ski@gmail.com)>
2023-09-12 08:19:56 +0200

    feat: add instrumentations1 slides

--- src/slides/04_GF101/Instrumentations1.tsx
+++ src/slides/04_GF101/Instrumentations1.tsx
@@ -1,67 +1,54 @@
-import { codePaneThemes, SlideLayout } from 'spectacle'
+import { CodeSpan, FlexBox, Heading, Image, ListItem, Slide, UnorderedList } from 'spectacle'
 import Back from '../../assets/images/back.png'
+import Logs from '../../assets/icons/logs.svg'
 
 export function Instrumentations1() {
   return (
-    <SlideLayout.MultiCodeLayout
-      numColumns={2}
-      backgroundImage={`url(${Back})`}
-      title={
-        <>
-          Instrumentation<span style={{ fontSize: 36 }}>(console)</span>
-        </>
-      }
-      titleProps={{ fontSize: '48px', textAlign: 'left' }}
-      codeBlocks={[
-        {
-          code: `
-          initializeFaro({
-            url: 'https://my-domain.my-tld/collect/{app-key}',
-            app: {
-              name: 'my-app',
-            },
-            instrumentations: [
-            ...getWebInstrumentations({
-              // Optional, disable the console instrumentation
-              captureConsole: false,
-
-              // Optional, collect all levels
-              captureConsoleDisabledLevels: [],
-
-              // Optional, disable only some levels
-              captureConsoleDisabledLevels: [LogLevel.DEBUG, LogLevel.TRACE],
-            })],
-          });
-  `,
-          language: 'jsx',
-          description: 'Config por defecto',
-          highlightRanges: [[1], [6], [7], [9], [12], [15]],
-          theme: codePaneThemes.a11yDark,
-        },
-        {
-          code: `
-          initializeFaro({
-            url: 'https://my-domain.my-tld/collect/{app-key}',
-            app: {
-              name: 'my-app',
-            },
-            instrumentations: [
-              new ConsoleInstrumentation({
-                // Optional collect all levels
-                disabledLevels: [],
-
-                // Optional disable only some levels
-                disabledLevels: [LogLevel.DEBUG, LogLevel.TRACE],
-              }),
-            ],
-          });
-  `,
-          language: 'jsx',
-          description: 'Config manual',
-          highlightRanges: [[6], [7], [9], [12]],
-          theme: codePaneThemes.a11yDark,
-        },
-      ]}
-    ></SlideLayout.MultiCodeLayout>
+    <Slide backgroundImage={`url(${Back})`}>
+      <FlexBox
+        width="100%"
+        height="100%"
+        flexDirection="column"
+        alignItems="flex-start"
+        justifyContent="flex-start"
+      >
+        <FlexBox width="100%" alignItems="center" justifyContent="flex-start">
+          <Heading fontSize="48px" margin="0">
+            Instrumentation<span style={{ fontSize: 36 }}>(console)</span>{' '}
+          </Heading>
+          <Image src={Logs} width="64px" />
+        </FlexBox>
+        <UnorderedList>
+          <ListItem>
+            Instrumentación: <CodeSpan color="secondary">getWebInstrumentations</CodeSpan>
+          </ListItem>
+          <ListItem>
+            Clase que lo implementa: <CodeSpan color="secondary">ConsoleInstrumentation</CodeSpan>
+          </ListItem>
+          <ListItem>¿Qué es lo que hace?</ListItem>
+          <UnorderedList>
+            <ListItem color="secondary">
+              Implementación custom de <CodeSpan>console</CodeSpan>
+            </ListItem>
+            <ListItem color="secondary">
+              Envía por defecto todos los <CodeSpan>console.error</CodeSpan>
+            </ListItem>
+          </UnorderedList>
+          <ListItem>¿Para qué nos sirve?</ListItem>
+          <UnorderedList>
+            <ListItem color="secondary">
+              Proveen de contexto para la detección de la causa raíz de los errores
+            </ListItem>
+            <ListItem color="secondary">
+              Capturan mensajes emitidos tanto por la aplicación como por las librerías
+            </ListItem>
+            <ListItem color="secondary">
+              Permite monitorizar los logs que usamos como bandera para comprobar el correcto
+              funcionamiento de la app
+            </ListItem>
+          </UnorderedList>
+        </UnorderedList>
+      </FlexBox>
+    </Slide>
   )
 }
